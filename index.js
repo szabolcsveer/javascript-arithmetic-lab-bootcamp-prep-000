bea28b49a92a91beb51912b488fcaa889f7b406f
Szabolcs Veer <futuremove@gmail.com>
2018-08-18 15:03:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add(a,b) {
+  return a + b ;
+}
+
+function subtract(a,b) {
+  return a - b ;
+}
+
+function multiply(a,b) {
+  return a * b ;
+}
+
+function divide(a,b) {
+  return a / b ;
+}
+
+function inc(n) {
+  return a+=2;
+}
+
+function inc(n) {
+  return n - 1 ;
+}
+
+
+
+
